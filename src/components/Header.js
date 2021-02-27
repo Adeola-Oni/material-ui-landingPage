71757c953354299c51edf7fb437c7241abe0f77f
Adeola-Oni <oniadeola33@gmail.com>
2021-02-27 13:15:44 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link as Scroll} from 'react-scroll'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        color: '#fff'
+    },
+    appbar: {
+        background: 'none',
+        fontFamily: 'nunito'
+    },
+    icon:{ 
+        color: '#fff',
+        fontSize: '2rem',
+    },
+    appbarTitle:{
+        flexGrow: 1,
+    },
+    appbarWrapper:{
+        width: '80%',
+        margin: '0 auto'
+    },
+    innerTitle:{
+        color: '#eeef20'
+    },
+    title: {
+        fontFamily: 'nunito',
+        fontSize: '2.5rem',
+        textAlign: 'center',
+    },
+    goDown: {
+        color: '#eeef20',
+        fontSize: '4rem'
+    }
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(()=>{
+      setChecked(true);
+  }, [])
+  return <div className={classes.root} id="header">
+      <AppBar className={classes.appbar} elevation={0}>
+          <Toolbar className={classes.appbarWrapper}>
+            <h1 className={classes.appbarTitle}>per<span className={classes.innerTitle}>SONY.</span></h1>
+            <IconButton>
+                <SortIcon className={classes.icon}/>
+            </IconButton>
+          </Toolbar>
+      </AppBar>
+      <Collapse in={checked} {...(checked ? {timeout: 1000}: {})} collapsedHeight={50}>
+        <div className={classes.title}>
+            <h1> Welcome to <br/>
+            per<span className={classes.innerTitle}>SONY.</span>
+            </h1>
+            <Scroll to='place-to-visit' smooth={true}>
+                <IconButton>
+                    <ExpandMoreIcon className={classes.goDown}/>
+                </IconButton>
+            </Scroll>
+        </div>
+      </Collapse>
+      
+    </div>
+  
+};
---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+    {
+        title:'Green',
+        description : "the maldives are a home for the most amazing creatures on earth. Take a look around and you'll see how amazing nature is.",
+        imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+        time: 1500,
+    },
+
+    {
+        title:'Bora Bora',
+        description : "Bora Bora is ana amzing island on the west coast of Santa Fex. It boasts of an amazing culture and delectabke cuisine,",
+        imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+        time: 1500,
+    },
+
+]
+
+export default places;
